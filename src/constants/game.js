a12ad02aabe527754cc4846b0a8780e722396e3d
Gonzalo Molina <gonzalo.molina@est.fi.uncoma.edu.ar>
2026-04-30 14:07:25 -0300

    feat: create .env.example for environment variable configuration and add game constants to centralize options

---
+++ src/constants/game.js
@@ -0,0 +1,19 @@
+/**
+ * Constantes de dominio para el juego HEXA.
+ * Estas opciones se utilizan en filtros, creación de cartas (IA) y validaciones.
+ */
+
+export const TYPE_OPTIONS = [
+  'creature',
+  'spell',
+  'artifact'
+];
+
+export const RARITY_OPTIONS = [
+  'poor',
+  'common',
+  'uncommon',
+  'rare',
+  'epic',
+  'legendary'
+];
